5d7d7d736b2db2e0a5c69381c62a8f9713a6e420
nicram <nicram@MyslacyDebian.home>
2026-05-05 10:47:18 +0200

    reorganize file structure

--- src/imports.py
+++ src/imports.py
@@ -1,24 +1,7 @@
+from faker import Faker
+import time 
 import os
 import sys
-import json
-import re
-import time
-import datetime
-import pathlib
-import argparse
-import subprocess
-import platform
-import shutil
-import logging
-import typer
-import requests
-from rich.console import Console
-from rich.table import Table
-from rich.panel import Panel
-from rich.progress import Progress, SpinnerColumn, TextColumn
-from rich.prompt import Prompt, Confirm
-from rich import print as rprint
-from dotenv import load_dotenv
-load_dotenv()
-console = Console()
-app = typer.Typer()
+from colorama import Fore, init, Back, Style
+import webbrowser
+init()
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,13 @@
 from src.imports import *
+from src.imports import *
 
-#Python Typing Text Effect - www.101computing.net/python-typing-text-effect/
-def typingPrint(text):
-   for character in text:
-      sys.stdout.write(character)
-      sys.stdout.flush()
-      time.sleep(0.05)
+
+SITE_LINK_1 = "https://nicram-code.github.io/"
+SITE_LINK_2 = "https://doxbean.cc/@nicram"
+
+
+
+
+def open_bio_sites():
+    webbrowser.open(SITE_LINK_1)
+    webbrowser.open(SITE_LINK_2)
